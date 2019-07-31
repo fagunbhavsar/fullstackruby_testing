3ef9518c7cd9795b7ddce77ce19d98c764b2f70e
Fagun Bhavsar <github.fagun@gmail.com>
2019-08-01 00:23:46 +0530

    Modified step and environment file

--- features/step_definitions/widget_steps.rb
+++ features/step_definitions/widget_steps.rb
@@ -1,5 +1,5 @@
 Given (/I am on the fsar widgets page/) do 
-  @browser.get("https://fullstackautomationwithruby.com/widgets")			
+  @widgets_index.visit
 end
 
 Then (/I verify widgets display on the page/) do
@@ -8,4 +8,9 @@ end
 
 Then (/I should see "(.*)" on the page/) do |page_text|
 	expect(@browser.find_element(:id, 'widgets').text).to include(page_text)
+end
+
+Then (/I verify the widgets table has "(.*)" rows/) do |row_count|
+  puts rows = @browser.find_elements(css: '#widgets_table tr').count - 1
+  rows.should == row.count.to_i
 end
--- features/support/env.rb
+++ features/support/env.rb
@@ -1,9 +1,11 @@
 require 'selenium-webdriver'
 require 'cukehub'
 require 'rspec'
+require_relative '../page_objects/widgets_index_page'
 
 @cukehub_api_key = "Gx9CeV3Zo31JwhhvmZ2LsYgu"
 
 Before do
 	@browser = Selenium::WebDriver.for :chrome
+  @widgets_index = WidgetsIndex.new(@browser)
 end
