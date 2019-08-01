8ec4b3888b5488b87323111350e9b5a3c7758f88
Fagun Bhavsar <github.fagun@gmail.com>
2019-08-01 15:24:46 +0530

    Deleted newly added page object due to unrelated feature

--- features/step_definitions/widget_steps_1.rb
+++
@@ -1,8 +0,0 @@
-Given(/^I am on the new widget page$/) do
-  @widget_new.visit
-end
-
-And (/^I fill out the new widget form$/) do
-  @widget_new.name.send_keys("BOT_DATA_WidgetName#{rand(999)}.to_s")
-  @widget_new.new.description.send_keys("BOT_DATA_WidgetDescription")
-end
